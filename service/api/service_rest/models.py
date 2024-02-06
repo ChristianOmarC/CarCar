4faf4761cabeff03879595fca10a71e4ca282955
Christian Culajay <culajay.christian@gmail.com>
2024-02-06 18:19:01 -0500

    started service models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    employee_id = models.PositiveSmallIntegerField()
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+
+class Appoinment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField()
+    status = models.BooleanField(default=False)
+    vin = models.CharField(max_length=17)
+    customer = models.CharField(max_length=50)
+
+    technician = models.ForeignKey(
+        Technician,
+        related_name=""
+    )
